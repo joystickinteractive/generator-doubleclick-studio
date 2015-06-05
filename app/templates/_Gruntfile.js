e42d0091f909d4e9f97f9f91aa52a54f7724336f
Mykhel Trinitaria <mykhel0003@yahoo.com>
2015-06-06 03:36:16 +0800

    Integrating sprites tool

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -15,6 +15,13 @@ module.exports = function(grunt) {
           livereload: '<' + '%= connect.options.livereload %>'
         }
       },
+      image: {
+        files: ['dev/sprites/*.png'],
+        tasks: ['sprite'],
+        options: {
+          livereload: '<' + '%= connect.options.livereload %>'
+        }
+      },
       livereload: {
         options: {
           livereload: '<' + '%= connect.options.livereload %>'
@@ -93,6 +100,48 @@ module.exports = function(grunt) {
         }]
       }
     },
+    // Reads HTML for usemin blocks to enable smart builds that automatically
+    // concat, minify and revision files. Creates configurations in memory so
+    // additional tasks can operate on them
+    useminPrepare: {
+      options: {
+        dest: 'pub/'
+      },
+      html: 'pub/index.html'
+    },
+
+    // Performs rewrites based on rev and the useminPrepare configuration
+    usemin: {
+      options: {
+        assetsDirs: [
+          'pub/'
+        ]
+      },
+      html: ['pub/{,*/}*.html'],
+      css: ['pub/{,*/}*.css']
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          removeAttributeQuotes: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: false,
+          removeOptionalTags: false,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeComments: true,
+        },
+        files: [{
+          expand: true,
+          cwd: 'pub/',
+          src: '{,*/}*.html',
+          dest: 'pub/'
+        }]
+      }
+    },
     copy: {
       dist: {
         files: [{
@@ -101,6 +150,19 @@ module.exports = function(grunt) {
           src: ['**', '!*.css', '!*.png', '!*.jpg'],
           dest: 'pub/'
         }]
+      }
+    },
+    sprite:{
+      all: {
+        src: 'dev/sprites/*.png',
+        dest: 'dev/spr.png',
+        destCss: 'dev/sprites.css',
+        padding: 2,
+        cssOpts: {
+          cssSelector: function (item) {
+            return '.' + item.name;
+          }
+        }
       }
     }
   });
@@ -112,10 +174,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-usemin');
+
+  // Load in `grunt-spritesmith`
+  grunt.loadNpmTasks('grunt-spritesmith');
 
   // Default task.
   grunt.registerTask('default', ['clean', 'copy', 'concat', 'imagemin']);
-  grunt.registerTask('build', ['clean', 'copy', 'cssmin', 'uglify', 'imagemin']);
+  grunt.registerTask('build', ['clean', 'sprite', 'copy', 'cssmin', 'uglify', 'imagemin', 'useminPrepare', 'usemin', 'htmlmin']);
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
